2c556a97a3b9f7378b708ee4f3cb885177d98e1d
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-10-23 05:02:30 -0500

    feat: add endpoint to search by id and letraid

--- src/routes/operation.js
+++ src/routes/operation.js
@@ -47,4 +47,31 @@ router.post('/create-operation', authMiddleware, async (req, res) => {
     }
 });
 
+// Get  para traer todas las operaciones de una letra
+router.get('/operations/:letraId', authMiddleware, async (req, res) => {
+    try {
+        const operations = await operationSchema.find({ letraId: req.params.letraId });
+        res.json(operations);
+    } catch (error) {
+        console.error('Error al obtener las operaciones:', error);
+        res.status(500).json({ message: 'Error al obtener las operaciones' });
+    }
+});
+
+// Get para traer una operacion por id
+router.get('/operation/:operationId', authMiddleware, async (req, res) => {
+    try {
+        const operation = await operationSchema.findById(req.params.operationId);
+        if (!operation) {
+            return res.status(404).json({ message: 'Operación no encontrada' });
+        }
+        res.json(operation);
+    } catch (error) {
+        console.error('Error al obtener la operación:', error);
+        res.status(500).json({ message: 'Error al obtener la operación' });
+    }
+});
+
+
+
 module.exports = router;
