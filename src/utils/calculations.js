6bf8a3ae01d0c257b4b5443539283c591a4f8a5e
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-10-23 04:39:49 -0500

    feat: add calculation logic different types of operations

---
+++ src/utils/calculations.js
@@ -0,0 +1,33 @@
+const calculatePeriodoDias = (fecha_vencimiento, fecha_descuento) => {
+    const diffTime = Math.abs(new Date(fecha_vencimiento) - new Date(fecha_descuento));
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+};
+
+const calculateTEAForPeriod = (tasaEfectivaAnual, periodo_dias) => {
+    return Math.pow((1 + tasaEfectivaAnual), (periodo_dias / 360)) - 1;
+};
+const calculateTasaDescontada = (TeaForPeriod) => {
+    return TeaForPeriod / (1 + TeaForPeriod);
+};
+
+const calculateValorRecibido = (valor_nominal, tasa_descontada, desgravamen) => {
+    const vneto = valor_nominal * (1-tasa_descontada);
+    return vneto - (desgravamen * valor_nominal);
+};
+
+const calculateValorEntregado = (valor_nominal) => {
+    return valor_nominal;
+};
+
+const calculateTCEA = (valor_entregado, valor_recibido, periodo_dias) => {
+    return Math.pow((valor_entregado / valor_recibido), (360 / periodo_dias)) - 1;
+};
+
+module.exports = {
+    calculatePeriodoDias,
+    calculateValorEntregado,
+    calculateValorRecibido,
+    calculateTEAForPeriod,
+    calculateTasaDescontada,
+    calculateTCEA
+};
